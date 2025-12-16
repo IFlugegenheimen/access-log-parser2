f51c44c065e52e1910c8d474cdff5f79e75a5de7
Mihail Parakevich <bizaro666@mail.ru>
2025-12-16 12:10:13 +0300

    обновление логики, было добавлено чтение строк при нахождении файла и выбрасывание исключений, если есть слишком большие строки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
+
+class LineTooLongException extends RuntimeException {
+    public LineTooLongException(String message) {
+        super(message);
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
@@ -18,8 +26,43 @@ public class Main {
             } else {
                 validFileCount++;
                 System.out.println("Путь указан верно. Это файл номер " + validFileCount);
+                
+                try {
+                    FileReader fileReader = new FileReader(path);
+                    BufferedReader reader = new BufferedReader(fileReader);
+                    
+                    int totalLines = 0;
+                    int maxLength = Integer.MIN_VALUE;
+                    int minLength = Integer.MAX_VALUE;
+                    String line;
+                    
+                    while ((line = reader.readLine()) != null) {
+                        int length = line.length();
+                        
+                        if (length > 1024) {
+                            throw new LineTooLongException("Обнаружена строка длиной " + length +
+                                    " символов, что превышает допустимый лимит в 1024 символа.");
+                        }
+                        
+                        totalLines++;
+                        if (length > maxLength) {
+                            maxLength = length;
+                        }
+                        if (length < minLength) {
+                            minLength = length;
+                        }
+                    }
+                    
+                    reader.close();
+                    
+                    System.out.println("Общее количество строк в файле: " + totalLines);
+                    System.out.println("Длина самой длинной строки: " + maxLength);
+                    System.out.println("Длина самой короткой строки: " + minLength);
+                    
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
             }
         }
     }
 }
-
